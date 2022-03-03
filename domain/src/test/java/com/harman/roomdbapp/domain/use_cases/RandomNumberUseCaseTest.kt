d46d09c85d58a9a43ce2dcb3fd12dc21de2f3ccb
andrzakharov <ntr.reyzaxarov@gmail.com>
2022-03-03 17:55:31 +0300

    included folders for unit tests in modules created initialized use case test class

---
+++ domain/src/test/java/com/harman/roomdbapp/domain/use_cases/RandomNumberUseCaseTest.kt
@@ -0,0 +1,18 @@
+package com.harman.roomdbapp.domain.use_cases
+
+import org.junit.Test
+
+
+internal class RandomNumberUseCaseTest {
+
+
+    @Test
+    fun addNumber() {
+
+    }
+
+    @Test
+    fun getNumbers() {
+        
+    }
+}
