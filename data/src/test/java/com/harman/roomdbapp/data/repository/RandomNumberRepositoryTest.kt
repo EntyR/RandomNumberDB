97714ade5145773f8989e84220f3251865acd544
andrzakharov <ntr.reyzaxarov@gmail.com>
2022-03-04 12:41:00 +0300

    Added idea in git ignore

---
+++ data/src/test/java/com/harman/roomdbapp/data/repository/RandomNumberRepositoryTest.kt
@@ -0,0 +1,14 @@
+package com.harman.roomdbapp.data.repository
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class RandomNumberRepositoryTest {
+
+    @org.junit.jupiter.api.Test
+    fun addNumber() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getNumbers() {
+    }
+}
