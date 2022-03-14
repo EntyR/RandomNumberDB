fa501fe938eccaab469dc56f37fd20f9d167eba1
andrzakharov <ntr.reyzaxarov@gmail.com>
2022-03-14 14:45:39 +0300

    changed add new item fragment ui

--- app/src/main/java/com/harman/roomdbapp/app/other/MathUtils.kt
+++ app/src/main/java/com/harman/roomdbapp/app/other/MathUtils.kt
@@ -1,6 +1,9 @@
 package com.harman.roomdbapp.app.other
 
+import android.content.Context
+
 object MathUtils {
 
     fun isNumberEven(number: Int) = number % 2 == 0
+
 }
--- app/src/main/java/com/harman/roomdbapp/app/ui/fragments/RandomNumberAddItem.kt
+++ app/src/main/java/com/harman/roomdbapp/app/ui/fragments/RandomNumberAddItem.kt
@@ -1,29 +1,71 @@
 package com.harman.roomdbapp.app.ui.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.RelativeLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.view.updateMargins
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doOnTextChanged
 import com.harman.roomdbapp.app.R
-
-private const val ARG_PARAM1 = "param1"
+import com.harman.roomdbapp.app.databinding.FragmentRandomNumberAddItemBinding
+import com.harman.roomdbapp.app.ui.viewmodel.NumberListViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 class RandomNumberAddItem : Fragment() {
 
+private  lateinit var binding: FragmentRandomNumberAddItemBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private val viewModel: NumberListViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_random_number_add_item, container, false)
+
+        binding = FragmentRandomNumberAddItemBinding.inflate(inflater, container, false)
+        val layoutParam = ConstraintLayout.LayoutParams(binding.btAddNewNumber.layoutParams as ConstraintLayout.LayoutParams)
+        binding.etEnterNumber.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (p0.isNullOrBlank()){
+                    val changedParams  = ConstraintLayout.LayoutParams(layoutParam)
+                    val horMargin = requireContext().resources.getDimensionPixelSize(R.dimen.add_number_btn_margin)
+                    changedParams.topToBottom = ConstraintLayout.LayoutParams.UNSET
+                    changedParams.startToStart = binding.root.id
+                    changedParams.endToEnd = binding.root.id
+                    changedParams.bottomToBottom = binding.root.id
+                    changedParams.setMargins(horMargin,0,horMargin,140)
+                    changedParams.height = ViewGroup.LayoutParams.WRAP_CONTENT
+                    changedParams.width = 0
+                    binding.btAddNewNumber.layoutParams = changedParams
+                    binding.btAddNewNumber.textSize = 21f
+                    binding.btAddNewNumber.minHeight = 190
+                }
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int)  {
+                if (p0.isNullOrBlank()){
+                    binding.btAddNewNumber.layoutParams = layoutParam
+                    binding.btAddNewNumber.textSize = 16f
+                    binding.btAddNewNumber.minHeight = 42
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) = Unit
+        })
+
+
+
+
+        return binding.root
     }
 
     companion object {
