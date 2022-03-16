4382713a1e93de489afe93081c38e3a0627c62a6
andrzakharov <ntr.reyzaxarov@gmail.com>
2022-03-16 17:55:09 +0300

    back button pressed test and verify value tests

--- app/src/androidTest/java/com/harman/roomdbapp/app/ui/fragments/RandomNumberDescriptionTest.kt
+++ app/src/androidTest/java/com/harman/roomdbapp/app/ui/fragments/RandomNumberDescriptionTest.kt
@@ -1,53 +1,69 @@
 package com.harman.roomdbapp.app.ui.fragments
 
-import androidx.core.os.bundleOf
+import android.util.Log
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.testing.FragmentScenario
-import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.fragment.app.testing.withFragment
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
 import com.harman.roomdbapp.app.R
-import com.harman.roomdbapp.app.other.NUMBER_VALUE
 import com.harman.roomdbapp.app.ui.MainActivity
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito.*
 
 @RunWith(AndroidJUnit4::class)
 class RandomNumberDescriptionTest {
 
     lateinit var activityScenario: ActivityScenario<MainActivity>
 
-//    @get:Rule
-//    val activityRule = ActivityScenarioRule(MainActivity::class.java)
-
-    lateinit var fragment: FragmentScenario<RandomNumberDescription>
-
-    lateinit var spy: FragmentManager
+    private lateinit var spy: FragmentManager
+    val value = 15
 
     @Before
     fun setUp() {
-        val bundle = bundleOf(NUMBER_VALUE to 1)
-        fragment = launchFragmentInContainer<RandomNumberDescription>(bundle)
-
-        fragment.withFragment {
-            spy = spy(parentFragmentManager)
-            fragment.withFragment {
-
-            }
+        activityScenario = ActivityScenario.launch(MainActivity::class.java).onActivity {
+            spy = it.supportFragmentManager
+            setUpFragment(it.supportFragmentManager, RandomNumbersList.newInstance(), "go_to_list")
+            navigateTo(spy, RandomNumberDescription.newInstance(value), "go_to_description")
         }
     }
 
     @Test
-    fun testBackButtonPressed() {
-        doNothing().`when`(spy).popBackStack()
+    fun testBackButtonIsPressed() {
+        Espresso.onView(withId(R.id.btBack)).perform(click())
+        assertThat(spy.backStackEntryCount).isEqualTo(0)
+    }
 
-        onView(withId(R.id.btBack)).perform(click())
-        verify(spy).popBackStack()
+    @Test
+    fun testNumberInDescriptionIsSameAsInArgument() {
+        onView(withId(R.id.tvNumberValue)).check(matches(withSubstring("15")))
+    }
+
+    @Test
+    fun testNumberDescriptionInDescriptionIsCorrect() {
+        onView(withId(R.id.tvNumberDescription)).check(matches(withText(R.string.number_not_even)))
+    }
+
+
+    private fun setUpFragment(parentFragmentManager: FragmentManager, fragmentToNavigate: Fragment, transactionName: String) {
+        val transaction = parentFragmentManager.beginTransaction()
+        transaction.add(R.id.fragmentContainerView, fragmentToNavigate)
+        transaction.commitAllowingStateLoss()
+    }
+    private fun navigateTo(parentFragmentManager: FragmentManager, fragmentToNavigate: Fragment, transactionName: String) {
+        val transaction = parentFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainerView, fragmentToNavigate)
+        transaction.addToBackStack(transactionName)
+        transaction.commitAllowingStateLoss()
     }
 }
