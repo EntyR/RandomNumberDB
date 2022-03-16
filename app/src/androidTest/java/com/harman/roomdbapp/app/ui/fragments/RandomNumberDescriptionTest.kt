dd9d6cf542a4b4aa4547e4dde8cd8a47a82a3292
andrzakharov <ntr.reyzaxarov@gmail.com>
2022-03-16 17:59:01 +0300

    lint fix

--- app/src/androidTest/java/com/harman/roomdbapp/app/ui/fragments/RandomNumberDescriptionTest.kt
+++ app/src/androidTest/java/com/harman/roomdbapp/app/ui/fragments/RandomNumberDescriptionTest.kt
@@ -1,19 +1,15 @@
 package com.harman.roomdbapp.app.ui.fragments
 
-import android.util.Log
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.testing.FragmentScenario
 import androidx.test.core.app.ActivityScenario
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withSubstring
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.google.common.truth.Truth
 import com.google.common.truth.Truth.assertThat
 import com.harman.roomdbapp.app.R
 import com.harman.roomdbapp.app.ui.MainActivity
@@ -33,14 +29,14 @@ class RandomNumberDescriptionTest {
     fun setUp() {
         activityScenario = ActivityScenario.launch(MainActivity::class.java).onActivity {
             spy = it.supportFragmentManager
-            setUpFragment(it.supportFragmentManager, RandomNumbersList.newInstance(), "go_to_list")
+            setUpFragment(it.supportFragmentManager, RandomNumbersList.newInstance())
             navigateTo(spy, RandomNumberDescription.newInstance(value), "go_to_description")
         }
     }
 
     @Test
     fun testBackButtonIsPressed() {
-        Espresso.onView(withId(R.id.btBack)).perform(click())
+        onView(withId(R.id.btBack)).perform(click())
         assertThat(spy.backStackEntryCount).isEqualTo(0)
     }
 
@@ -54,13 +50,20 @@ class RandomNumberDescriptionTest {
         onView(withId(R.id.tvNumberDescription)).check(matches(withText(R.string.number_not_even)))
     }
 
-
-    private fun setUpFragment(parentFragmentManager: FragmentManager, fragmentToNavigate: Fragment, transactionName: String) {
+    private fun setUpFragment(
+        parentFragmentManager: FragmentManager,
+        fragmentToNavigate: Fragment
+    ) {
         val transaction = parentFragmentManager.beginTransaction()
         transaction.add(R.id.fragmentContainerView, fragmentToNavigate)
         transaction.commitAllowingStateLoss()
     }
-    private fun navigateTo(parentFragmentManager: FragmentManager, fragmentToNavigate: Fragment, transactionName: String) {
+
+    private fun navigateTo(
+        parentFragmentManager: FragmentManager,
+        fragmentToNavigate: Fragment,
+        transactionName: String
+    ) {
         val transaction = parentFragmentManager.beginTransaction()
         transaction.replace(R.id.fragmentContainerView, fragmentToNavigate)
         transaction.addToBackStack(transactionName)
