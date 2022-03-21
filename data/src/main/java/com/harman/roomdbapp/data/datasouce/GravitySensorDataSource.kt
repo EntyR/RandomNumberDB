5903b9aec7ced8924c7134548392053c5f4a2e5c
andrzakharov <ntr.reyzaxarov@gmail.com>
2022-03-21 12:17:27 +0300

    update gravity datasource to callback flow

--- data/src/main/java/com/harman/roomdbapp/data/datasouce/GravitySensorDataSource.kt
+++ data/src/main/java/com/harman/roomdbapp/data/datasouce/GravitySensorDataSource.kt
@@ -9,46 +9,49 @@ import com.harman.roomdbapp.domain.datasource.IGravityCensorDataSource
 import com.harman.roomdbapp.domain.model.GravityValue
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.SendChannel
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.onFailure
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.internal.ChannelFlow
 import kotlinx.coroutines.launch
 
 class GravitySensorDataSource(val context: Context) : IGravityCensorDataSource {
     private val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
     private val sensor: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)
 
-    private val fluctuationsFlow: MutableStateFlow<List<GravityValue>> =
-        MutableStateFlow(emptyList())
-    private val scope = CoroutineScope(Dispatchers.Default)
 
-    init {
-        sensorManager.registerListener(
-            object : SensorEventListener {
-                override fun onSensorChanged(event: SensorEvent?) {
-                    val value = event?.values
-                    value?.let { vector ->
-                        val gravValue = GravityValue(
-                            value[0],
-                            value[1],
-                            value[2]
-                        )
-                        scope.launch(Dispatchers.Default) {
-                            val list = fluctuationsFlow.value.toMutableList()
-                            list.add(gravValue)
-                            fluctuationsFlow.value = list
-                        }
-                    }
+    override suspend fun getCensorFlow() = callbackFlow {
+
+        val list: MutableList<GravityValue> = mutableListOf()
+
+        val listener = object : SensorEventListener {
+            override fun onSensorChanged(event: SensorEvent?) {
+                val value = event?.values
+                value?.let { vector ->
+                    val gravValue = GravityValue(
+                        value[0],
+                        value[1],
+                        value[2]
+                    )
+                    list.add(gravValue)
+                    trySend(list)
                 }
+            }
 
-                override fun onAccuracyChanged(p0: Sensor?, p1: Int) = Unit
-            },
+            override fun onAccuracyChanged(p0: Sensor?, p1: Int) = Unit
+        }
+
+        sensorManager.registerListener(
+            listener,
             sensor,
             500000
         )
-    }
-
-    override suspend fun getCensorEventsFlow(): StateFlow<List<GravityValue>> {
-
-        return fluctuationsFlow
+        awaitClose {
+            sensorManager.unregisterListener(listener, sensor)
+        }
     }
 }
--- domain/src/main/java/com/harman/roomdbapp/domain/datasource/IGravityCensorDataSource.kt
+++ domain/src/main/java/com/harman/roomdbapp/domain/datasource/IGravityCensorDataSource.kt
@@ -1,9 +1,10 @@
 package com.harman.roomdbapp.domain.datasource
 
 import com.harman.roomdbapp.domain.model.GravityValue
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
 
 interface IGravityCensorDataSource {
 
-    suspend fun getCensorEventsFlow(): StateFlow<List<GravityValue>>
+    suspend fun getCensorFlow(): Flow<List<GravityValue>>
 }
