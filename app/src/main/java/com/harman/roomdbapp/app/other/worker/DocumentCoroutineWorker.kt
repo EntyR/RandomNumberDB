aaf2cec8fe83c2039e2d492bb4048d7a0eeb9e67
Andrei <ntr.reyzaxarov@gmail.com>
2022-04-11 15:50:54 +0300

    change worker

--- app/src/main/java/com/harman/roomdbapp/app/other/worker/DocumentCoroutineWorker.kt
+++ app/src/main/java/com/harman/roomdbapp/app/other/worker/DocumentCoroutineWorker.kt
@@ -7,9 +7,9 @@ import androidx.work.WorkerParameters
 import com.harman.roomdbapp.app.other.MemoryUtil
 import com.harman.roomdbapp.app.other.MemoryUtil.Companion.convertToCsv
 import com.harman.roomdbapp.data.dao.IFluctuationDao
+import com.harman.roomdbapp.data.enity.FluctuationEntity
 import com.harman.roomdbapp.data.toGravityRecord
 import kotlinx.coroutines.flow.first
-import java.util.UUID
 
 class DocumentCoroutineWorker(
     val context: Context,
@@ -25,27 +25,46 @@ class DocumentCoroutineWorker(
         if (value.isNullOrEmpty())
             return Result.failure()
 
-        val lastItem = value.last().toGravityRecord().convertToCsv()
+        val lastItemCsv = value.last().toGravityRecord().convertToCsv()
 
-        if (lastItem == memoryUtil.getLastRecord()) {
-            // TODO delete when start displaying value in fragment
-            logValue()
-            return Result.success()
-        } else {
-            memoryUtil.saveToSharedPref(lastItem)
-            memoryUtil.addNewCsvFile(
-                "${UUID.randomUUID()}.csv",
-                value.map {
-                    it.toGravityRecord()
-                }
-            )
+        val lastDbEntry = value.find {
+            it.toGravityRecord().convertToCsv() == memoryUtil.getLastRecord()
         }
-        // TODO delete when start displaying value in fragment
-        logValue()
 
-        return Result.success()
+        when {
+            lastItemCsv == memoryUtil.getLastRecord() -> {
+                logValue()
+                return Result.success()
+            }
+            lastDbEntry != null -> {
+                val list = value.dropWhile {
+                    it != lastDbEntry
+                }
+                addNewRecords(list)
+                logValue()
+                return Result.success()
+
+            }
+            else -> {
+                addNewRecords(value)
+                logValue()
+                return Result.success()
+
+            }
+        }
     }
 
+    private  fun addNewRecords(value: List<FluctuationEntity>) {
+        memoryUtil.saveToSharedPref(value.last().toGravityRecord().convertToCsv())
+        memoryUtil.addNewCsvFile(
+            "${value.last().timestamp}.csv",
+            value.map {
+                it.toGravityRecord()
+            }
+        )
+    }
+
+    // TODO delete all logs when start displaying value in fragment
     private suspend fun logValue() {
         memoryUtil.getCsvList().last().let {
             Log.d("TAG", "Csv document: $it")
